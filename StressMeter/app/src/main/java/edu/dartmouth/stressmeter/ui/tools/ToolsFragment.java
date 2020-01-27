f1de751e4e48f05be5b7189e66dd4699fcc1683d
Pengze Liu <lpzjerry@gmail.com>
2020-01-26 22:57:24 -0500

    CSV display

--- StressMeter/app/src/main/java/edu/dartmouth/stressmeter/ui/tools/ToolsFragment.java
+++ StressMeter/app/src/main/java/edu/dartmouth/stressmeter/ui/tools/ToolsFragment.java
@@ -1,24 +1,61 @@
 package edu.dartmouth.stressmeter.ui.tools;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import android.widget.Toast;
+import android.util.Log;
+import android.content.res.AssetManager;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import edu.dartmouth.stressmeter.BigImgActivity;
 import edu.dartmouth.stressmeter.R;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class ToolsFragment extends Fragment {
+    private String filename = "stress_timestamp.csv";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_tools, container, false);
+        TextView textView = root.findViewById(R.id.csv_display);
+        AssetManager am = getActivity().getAssets();
+        String data = readFromFile(getContext());
+        Log.d("pengze", data);
+        textView.setText(data);
         return root;
+    }
+
+    private String readFromFile(Context context) {
+        String ret = "";
+        try {
+            InputStream inputStream = context.openFileInput(filename);
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString).append("\n");
+                }
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        } catch (Exception e) {
+            Log.d("pengze", "File read failed: " + e.toString());
+        }
+        return ret;
     }
 }
